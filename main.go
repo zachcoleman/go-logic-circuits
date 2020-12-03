4757688874d00e26fe30b7d72910fbd922ca645e
Zach Coleman <zachcoleman@Zachs-MacBook-Pro.local>
2020-12-02 18:05:13 -0600

    fixed deadlocking

--- main.go
+++ main.go
@@ -12,36 +12,41 @@ func main() {
 	// for testing primarily
 	rand.Seed(time.Now().UnixNano())
 
-	// will need to switch to map implementations (unique variable stuff for when text is parsed)
+	// will want to switch to map implementations (unique variable stuff for when text is parsed)
 
-	inputNodes := []Node{
-		NewSourceNode(),
-		NewSourceNode(),
-		NewSourceNode(),
-		NewSourceNode(),
+	inputNodes := []Node{}
+	for i := 0; i < 4; i++ {
+		tmp := NewSourceNode()
+		inputNodes = append(inputNodes, &tmp)
 	}
 
+	// slices to hold nodes
 	logicNodes := []Node{}
 	outputNodes := []Node{}
 
-	var newNode LogicNode
-
 	// this is pattern for putting node in graph
-	newNode = NewLogicNode(2, And)
-	logicNodes = append(logicNodes, Connect(inputNodes[:2], &newNode))
+	// can not use same variable since need new addresses
+	tmp1 := NewLogicNode(2, And)
+	logicNodes = append(logicNodes, Connect(inputNodes[:2], &tmp1))
 
-	newNode = NewLogicNode(2, And)
-	logicNodes = append(logicNodes, Connect(inputNodes[2:], &newNode))
+	tmp2 := NewLogicNode(2, Or)
+	logicNodes = append(logicNodes, Connect(inputNodes[2:], &tmp2))
 
-	newNode = NewLogicNode(2, Or)
-	logicNodes = append(logicNodes, Connect(logicNodes[:2], &newNode))
-	outputNodes = append(outputNodes, &newNode)
+	tmp3 := NewLogicNode(2, Xor)
+	logicNodes = append(logicNodes, Connect(logicNodes[:2], &tmp3))
+	outputNodes = append(outputNodes, &tmp3)
+
+	// Set all output nodes to push
+	for _, node := range outputNodes {
+		node.incrementOutput()
+	}
 
 	// push Bit onto all the sources
 	// right now all sources can only be used once!!!
 	for _, node := range inputNodes {
 		go func(n Node) {
-			n.getOutputChan() <- Bit(rand.Intn(2) == 1)
+			n.(*SourceNode).SetBitValue(Bit(rand.Intn(2) == 1)) // set random Bit
+			n.EvaluateNode()                                    // Start pushing them out
 		}(node)
 	}
 
@@ -49,6 +54,13 @@ func main() {
 	for _, node := range logicNodes {
 		go node.EvaluateNode()
 	}
+
+	// print inputs
+	inVals := []Bit{}
+	for _, node := range inputNodes {
+		inVals = append(inVals, node.(*SourceNode).bitValue)
+	}
+	fmt.Println(inVals)
 
 	// collect outputs
 	for _, node := range outputNodes {
