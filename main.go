4594b6ae321425cb0be2371eb43c3e92f525505b
Zachary Coleman <zacharywcoleman@gmail.com>
2021-09-15 23:40:05 -0500

    updated

--- main.go
+++ main.go
@@ -1,87 +1,175 @@
 package main
 
 import (
+	"bufio"
+	"errors"
+	"flag"
 	"fmt"
 	"go-logic-circuits/circuit"
 	"go-logic-circuits/gates"
 	"log"
 	"math/rand"
-	"time"
+	"os"
+	"strings"
 )
-
-/*
-TODO:
-	(x) create performance tests
-	() make text parser
-	() create map implementation instead of slice
-*/
 
 func main() {
 
 	// set random seed for pushing random values onto the sources
 	// for testing primarily
-	rand.Seed(time.Now().UnixNano())
+	// rand.Seed(time.Now().UnixNano())
 
+	// parse input
+	fp := flag.String("filepath", "",
+		"string filepath to circuit definition")
+	flag.Parse()
+
+	// open file
+	f, err := os.Open(*fp)
+	if errors.Is(err, os.ErrNotExist) {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	// create circuit
 	circ := circuit.NewCircuit()
 
-	fmt.Println(circ.ParseString("INPUT A B"))
-	fmt.Println(circ.ParseString("C = A OR B"))
-
-	fmt.Println(circ.ParseString("OUTPUT C"))
-
-	fmt.Println(circ.ParseString("INPUT D"))
-	fmt.Println(circ.ParseString("E = C AND D"))
-
-	fmt.Println(circ.ParseString("OUTPUT E"))
-
-	input := make(map[string]gates.Bit)
-	output := make(map[string]gates.Bit)
-
-	runs := 100
-	start := time.Now()
-	for i := 0; i < runs; i++ {
-		for name := range circ.InputNodes {
-			input[name] = gates.Bit(rand.Intn(2) == 1)
+	// read in the input
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		_, err := circ.ParseString(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
 		}
-		output = circ.Evaluate(input)
 	}
 
-	avgTime := time.Since(start) / time.Duration(runs)
+	// get a counter
+	outputData := make(map[string]float32)
+	for key := range circ.OutputNodes {
+		outputData[key] = 0.0
+	}
 
-	// log.Printf("\n\t" + strings.Join(circ.Log, "\n\t"))
-	log.Printf(`
-	Input: %v
-	Result: %v
-	Num input nodes: %v
-	Num output nodes: %v
-	Total number of operations: %v
-	Avg. Execution time (%v runs): %v`, input, output, len(circ.InputNodes), len(circ.OutputNodes),
-		len(circ.CircuitNodes), runs, avgTime)
+	// if number of input nodes > 10 then run random
+	// sample on circuit
+	if len(circ.CircuitNodes) > 10 {
 
-	// Duplicating circuit
-	// newCirc := circuit.DuplicateCircuit(circ)
+		// make maps
+		input := make(map[string]gates.Bit)
+		output := make(map[string]gates.Bit)
 
-	// start = time.Now()
-	// output = newCirc.Evaluate(input)
-	// elapsed = time.Since(start)
+		// do runs
+		runs := 1_000
+		for i := 0; i < runs; i++ {
+			for name := range circ.InputNodes {
+				input[name] = gates.Bit(rand.Intn(2) == 1)
+			}
+			output = circ.Evaluate(input)
 
-	// add a test that verifies that the Duplicate pointers point to different
-	// memory locations but produce same output (for random input)
-	// fmt.Println(circ.InputNodes)
-	// fmt.Println(newCirc.InputNodes)
-	// fmt.Println(circ.CircuitNodes)
-	// fmt.Println(newCirc.CircuitNodes)
-	// fmt.Println(circ.OutputNodes)
-	// fmt.Println(newCirc.OutputNodes)
+			for k, v := range output {
+				if v {
+					outputData[k] += 1.0
+				}
+			}
+		}
+		// get the freq data
+		for key := range circ.OutputNodes {
+			outputData[key] = outputData[key] / float32(runs)
+		}
 
-	// log.Printf("\n\t" + strings.Join(newCirc.Log, "\n\t"))
-	// log.Printf(`
-	// Input: %v
-	// Result: %v
-	// Num input nodes: %v
-	// Num output nodes: %v
-	// Total number of operations: %v
-	// Execution time: %v`, input, output, len(newCirc.InputNodes), len(newCirc.OutputNodes),
-	// 	len(newCirc.CircuitNodes), elapsed)
+		inputKeys := make([]string, 0, len(circ.InputNodes))
+		for key := range circ.InputNodes {
+			inputKeys = append(inputKeys, key)
+		}
 
+		log.Printf("\n\t" + strings.Join(circ.Log, "\n\t"))
+		log.Printf("\n\tInput(s): %v \n\tResults: %v",
+			inputKeys, outputData)
+
+	} else {
+		// run an enumeration
+		tree := makeTree(len(circ.InputNodes) + 1)
+		perms := traverseTree(tree, []int{})
+
+		// get one set ordering of key
+		keys := []string{}
+		for key := range circ.InputNodes {
+			keys = append(keys, key)
+		}
+
+		// get pairs of results
+		pairMap := [][]map[string]gates.Bit{}
+		for _, perm := range perms {
+			input := make(map[string]gates.Bit)
+			for i, key := range keys {
+				fmt.Println(i, gates.Bit(perm[i] == 1))
+				input[key] = gates.Bit(perm[i] == 1)
+			}
+			output := circ.Evaluate(input)
+			pairMap = append(pairMap, []map[string]gates.Bit{input, output})
+		}
+
+		log.Printf("\n\t" + strings.Join(circ.Log, "\n\t"))
+		logMsg := "\nInput:" + strings.Repeat(" ", len(pairMap[0][0])*5) + "\t\t\tResult:"
+		for _, pair := range pairMap {
+			logMsg += fmt.Sprintf("\n%v \t\t%v", pair[0], pair[1])
+		}
+		log.Printf(logMsg)
+	}
+}
+
+type Node struct {
+	val   int
+	left  *Node
+	right *Node
+}
+
+func fillTree(root *Node, num int) *Node {
+	if num < 1 {
+		return root
+	}
+
+	if num > 1 {
+		root.left = &Node{
+			0,
+			nil,
+			nil,
+		}
+		root.right = &Node{
+			1,
+			nil,
+			nil,
+		}
+	}
+	fillTree(root.left, num-1)
+	fillTree(root.right, num-1)
+
+	return root
+}
+
+func makeTree(n int) *Node {
+	root := &Node{
+		-1,
+		nil,
+		nil,
+	}
+	fillTree(root, n)
+	return root
+}
+
+func traverseTree(root *Node, curr []int) [][]int {
+	res := [][]int{}
+
+	if root.left == nil && root.right == nil {
+		curr = append(curr, root.val)
+		res = append(res, curr)
+		return res
+	} else {
+		if root.val != -1 {
+			curr = append(curr, root.val)
+		}
+		res = append(res, traverseTree(root.left, curr)...)
+		res = append(res, traverseTree(root.right, curr)...)
+	}
+
+	return res
 }
